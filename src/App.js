c30b740679e5be7aae4587ce252531d5d4f96508
nciampelletti <natashat@shaw.ca>
2022-04-19 07:29:25 -0700

    Add error, about, navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 // import pages
 import Home from "./pages/Home"
 import About from "./pages/About"
 import SingleCocktail from "./pages/SingleCocktail"
 import Error from "./pages/Error"
 // import components
-import Navbar from "./components/Navbar"
+
+import PageLayout from "./pages/PageLayout"
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-      <h2>this is my change</h2>
-      <h2>this is my 3 rd change</h2>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<PageLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contail/:id' element={<SingleCocktail />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        quasi, odio facere quibusdam corrupti aperiam similique sint, architecto
+        repellendus eveniet numquam nobis exercitationem officiis ipsum, maxime
+        explicabo id beatae ullam.
+      </p>
+    </section>
   )
 }
 
